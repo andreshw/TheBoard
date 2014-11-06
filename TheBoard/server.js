c6e96df885061345319f2bd4d46f38a3a1c27f2b
Andres Hurtado <andreshw@gmail.com>
2014-11-06 15:31:43 -0500

    Aplicacion web en nuget

---
+++ TheBoard/server.js
@@ -0,0 +1,11 @@
+﻿var http = require('http');
+var express = require('express');
+var app = express();
+
+app.get("/", function (request, response) { 
+    response.send('<html><body><h1>Hola Mundo!!!</h1></body></html>')
+});
+
+var server = http.createServer(app);
+
+server.listen(3000);
