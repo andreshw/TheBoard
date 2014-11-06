284c6ba10ecccbba6e0438ce9d5dd422b0551785
Andres Hurtado <andreshw@gmail.com>
2014-11-06 15:55:00 -0500

    add jade view engine

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -6,6 +6,11 @@ app.get("/", function (request, response) {
     response.send('<html><body><h1>Hola Mundo!!!</h1></body></html>')
 });
 
+app.get("/users/", function (request, response) { 
+    response.set("Content-Type", "application/json");
+    response.send({ nombre: "andres", profesion: "programador" });
+});
+
 var server = http.createServer(app);
 
 server.listen(3000);
